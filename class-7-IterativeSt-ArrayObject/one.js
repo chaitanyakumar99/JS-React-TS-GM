b098f825b9c63db5f0723c8ab13c2066afe282c3
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-11-16 10:40:51 +0530

    sat vibes

---
+++ class-7-IterativeSt-ArrayObject/one.js
@@ -0,0 +1,7 @@
+const someObject = { name: 'Mike',eid:1001,esal:45000 };
+
+/* for (let key of Object.keys(someObject)) {
+console.log(key + ": " + someObject[key]);
+}
+ */
+console.log(Object.keys(someObject))
