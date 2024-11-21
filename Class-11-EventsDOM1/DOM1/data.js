7328abde323b8fd40e53e4f13a692aecabd89bd6
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-11-21 10:06:39 +0530

    okie

---
+++ Class-11-EventsDOM1/DOM1/data.js
@@ -0,0 +1,5 @@
+/* let tag_Ref=document.getElementById('abc')
+console.log(tag_Ref)
+tag_Ref.innerHTML="Good Morning" */
+//document.getElementById('abc').innerHTML="Good Morning"
+document.getElementById('abc').innerHTML = new Date()
---
+++ Class-11-EventsDOM1/DOM2/data.js
@@ -0,0 +1,5 @@
+function changeColor(){
+    //alert("Good Morning")
+    //document.getElementById('abc').innerHTML="GM"
+    document.getElementById('abc').style.backgroundColor="Green"
+}
