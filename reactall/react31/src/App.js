68d472f412ba074b299e9c0f5ac7912fec497f8a
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-12-16 10:15:29 +0530

    T

--- reactall/react31/src/App.js
+++ reactall/react31/src/App.js
@@ -1,10 +1,14 @@
-import Message from "./LifeCycle/Message"
+//import Message from "./LifeCycle/Message"
+
+import Users from "./Users/Users"
+
 let App = ()=>{
+ 
 
     return <div>
               <h1>App component</h1>
               <hr/>
-            <Message/>
+            <Users/>
               <hr/>
             
            </div>
